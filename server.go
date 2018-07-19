712afc2decd6c9bce2a1331ff41ae0a78e74af53
sasaki-hiroaki <sasaki-hiroaki@dmm.com>
2018-07-19 12:31:07 +0900

    add template syntax

--- server.go
+++ server.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
 
+// Template ...
+type Template struct {
+	templates *template.Template
+}
+
+// Render ...
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+// Sample Handler ...
+func Sample(c echo.Context) error {
+	return c.Render(http.StatusOK, "sample", "Sampleでぇす!!!!")
+}
+
 func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+
 	e := echo.New()
+	e.Renderer = t
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Start(":8080")
+
+	e.GET("/sample", Sample)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
